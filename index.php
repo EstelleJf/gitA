cd21eee02096d168b57eb9e5829f8d6a94cc4023
Lucy <lisaji2010@sina.com>
2019-10-16 15:14:56 +0800

    chuangjian controller

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jfa
+ * Date: 2019/10/16
+ * Time: 15:12
+ */
---
+++ web/controller/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jfa
+ * Date: 2019/10/16
+ * Time: 15:13
+ */
+namespace  web\controller;
+class Index{
+    public function show(){
+        echo 'hdphp ...';
+    }
+}
